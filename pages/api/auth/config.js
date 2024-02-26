9051498561fe07166c8104c034cb3bc17f93ce96
Deepak Singh <54103399+dksr789@users.noreply.github.com>
2024-02-27 05:10:57 +0530

    Update config.js
    new changes publish

--- pages/api/auth/config.js
+++ pages/api/auth/config.js
@@ -1,4 +1,4 @@
 // config.js
-export const NEXTAUTH_URL = "https://testspo.vercel.app";
+export const NEXTAUTH_URL = "https://finaltestspo.vercel.app";
 export const SPOTIFY_CLIENT_ID = "e8df63d4390d4fd8a7cfefb4fc60dcee";
 export const SPOTIFY_CLIENT_SECRET = "be5d8be8754441b2a32017b68be5a4fa";
